238f92d17b4fb85d67a6b504178dc9de58c3a48b
Minindu Pasan <minindupasan@gmail.com>
2024-09-26 22:58:46 +0530

    Refactor and remove unused code files

--- app/Amali/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default function HelloAmali() {
-  return (
-    <div>
-        <h1>Hello Amali</h1></div>
-  )
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import {Button} from '@/components/ui/button'
 import Link from 'next/link'
+import { div } from 'framer-motion/client'
 
 export default function page() {
   return (
-    <div className="flex space-x-4 bg-slate-600">
-      <Link href="/Amali"> <Button className='bg-gray-950 p-12'>Amali</Button></Link>
-      <Link href="/Piggy"> <Button className='bg-gray-950 p-12'>Piggy</Button></Link>
-    </div>
+    div
   )
 }
