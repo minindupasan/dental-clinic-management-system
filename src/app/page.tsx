de2171098ad06d3090f65f2e714599e2c82dd496
Minindu Pasan <minindupasan@gmail.com>
2024-09-28 01:04:00 +0530

    Refactor NavBar component and remove unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Button } from "@radix-ui/themes";
-import Link from "next/link";
 
 export default function NavBar() {
   return <div className="text-center">This is Home</div>;
